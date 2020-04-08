6beffc5ae9053c8dc33594e9282e4f5cb9ce23fd
AnjaLumholtz <aeln12@student.aau.dk>
2020-04-08 19:05:39 +0200

    Foreløbig kode

--- cfg/buildAstVisitor.java
+++ cfg/buildAstVisitor.java
@@ -1,25 +1,56 @@
-package com.antlr;
-
-import roboBaseVisitor.class;
-import ExpressionNode.class;
 import java.util.function.Function;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 
 class BuildAstVisitor extends roboBaseVisitor<ExpressionNode>
 {
-	@Override
-	public ExpressionNode VisitCompileUnit(roboParser.CompileUnitContext context)
+    @Override
+	public ExpressionNode visitProgram(roboParser.ProgramContext context)
 	{
-		return Vist(context.expr());
+		return visit(context);
 	}
 
-	@Override
-	public ExpressionNode VisitNumberExpr(roboParser.NumberExprContext context)
+	public ExpressionNode visitDigitExpr(roboParser.DigitExprContext context)
 	{
+        NumberNode node = new NumberNode();
+
 		return new NumberNode()
 		{
-			Double Value = Double.Parse(context.value.Text, NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent);
+			node.Value = Double.Parse(context.value.Text, NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent);
 		};
 	}
+
+    @Override
+    public ExpressionNode visitInfixExpr(roboParser.InfixExprContext context)
+    {
+        InfixExpressionNode node;
+
+        switch (context.op.Type)
+        {
+            case roboLexer.ADD_OP:
+                node = new AdditionNode();
+                break;
+
+            case roboLexer.SUB_OP:
+                node = new SubtractionNode();
+                break;
+
+            case roboLexer.MUL_OP:
+                node = new MultiplicationNode();
+                break;
+
+            case roboLexer.DIV_OP:
+                node = new DivisionNode();
+                break;
+
+            default:
+                throw new NotSupportedException();
+        }
+
+        node.Left = visit(context.left);
+        node.Right = visit(context.right);
+
+        return node;
+    }
 }
